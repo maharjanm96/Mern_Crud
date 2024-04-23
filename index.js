cb86ff6db7fdac2a2202de195d425dc6ac6894a6
Manish Maharjan <maharjanm96@gmail.com>
2024-04-23 15:50:14 +0545

    Server Created

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hello Mern Crud");
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("Hello Mern Crud").status(200);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started at port http://localhost:${PORT}`);
+});
