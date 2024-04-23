600ffe8c8fe10f2e3f9fd8c8b603adc28033d434
Manish Maharjan <maharjanm96@gmail.com>
2024-04-23 20:34:54 +0545

    GetUsers

---
+++ controllers/getUsers.js
@@ -0,0 +1,24 @@
+const UserModel = require("../models/userModel");
+
+const getAllUsers = async (req, res) => {
+  try {
+    const allUser = await UserModel.find({});
+    res.status(200).json(allUser);
+  } catch (error) {
+    console.log("Something went wrong", error).status(400);
+  }
+};
+
+const getSingleUser = async (req, res) => {
+  try {
+    const { userId } = req.params;
+
+    const singleUser = await UserModel.findOne({ _id: userId });
+    res.json(singleUser ? singleUser : "No user");
+  } catch (error) {
+    res.status(400).json("Something went wrong");
+    console.log("Something went wrong", error);
+  }
+};
+
+module.exports = { getAllUsers, getSingleUser };
