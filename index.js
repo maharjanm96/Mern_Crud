b72ccbe1719c4e1fa9f9e99d7bf8f61d51d5b5dd
Manish Maharjan <maharjanm96@gmail.com>
2024-05-30 19:43:12 +0545

    Deployment

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const dbConnect = require("./dbConfig/db");
 require("dotenv").config();
 dbConnect();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 const user = require("./routes/userRoute");
 
