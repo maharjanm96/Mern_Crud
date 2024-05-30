5e63135bd1d5d4c173fc95784231a4caf29a8e56
Manish Maharjan <maharjanm96@gmail.com>
2024-05-30 19:02:50 +0545

    Deployment Changes

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ app.use(express.json());
 //routes
 app.use("/api/users", user);
 
+app.get("/", "This is a default route");
+
 app.listen(PORT, () => {
   console.log(`Server started at port http://localhost:${PORT}`);
 });
