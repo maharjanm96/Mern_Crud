478d08860abcfd6b3a5a8d1b55ea0dbc3be72a9a
Manish Maharjan <maharjanm96@gmail.com>
2024-06-10 21:11:57 +0545

    Update vercel.json and optimize functions

--- controllers/getUsers.js
+++ controllers/getUsers.js
@@ -5,7 +5,8 @@ const getAllUsers = async (req, res) => {
     const allUser = await UserModel.find({});
     res.status(200).json(allUser);
   } catch (error) {
-    console.log("Something went wrong", error).status(400);
+    console.error("Something went wrong", error);
+    res.status(500).json({ message: "Server error" });
   }
 };
 
@@ -13,10 +14,12 @@ const getSingleUser = async (req, res) => {
   const { userId } = req.params;
   try {
     const singleUser = await UserModel.findOne({ _id: userId });
-    res.json(singleUser ? singleUser : "No user");
+    res
+      .status(200)
+      .json(singleUser ? singleUser : { message: "No user found" });
   } catch (error) {
-    res.status(400).json("Something went wrong");
-    console.log("Something went wrong", error);
+    console.error("Something went wrong", error);
+    res.status(500).json({ message: "Server error" });
   }
 };
 
--- index.js
+++ index.js
@@ -2,18 +2,20 @@ const express = require("express");
 const cors = require("cors");
 const dbConnect = require("./dbConfig/db");
 
+const app = express();
+
 require("dotenv").config();
 dbConnect();
 
 const PORT = process.env.PORT;
 
-const user = require("./routes/userRoute");
 
-const app = express();
-app.use(cors());
+
 app.use(express.json());
+app.use(cors());
 
 //routes
+const user = require("./routes/userRoute");
 app.use("/api/users", user);
 
 app.get("/", (req, res) => {
